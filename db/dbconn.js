2372ede5b0ca49e73cc8712fb8a0b69a06a9ddff
Uhtred <shrestha8summit@gmail.com>
2024-08-06 21:18:09 +0530

    dbconn.js

--- db/dbconn.js
+++ db/dbconn.js
@@ -6,7 +6,11 @@ const mongoose = require("mongoose")
 function conn()
 {   
 try {
-    mongoose.connect(process.env.mango).then(console.log("Db connected."))
+    mongoose.connect(process.env.mango,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 50000, // Timeout after 50 seconds
+})).then(console.log("Db connected."))
 
 } catch (error) {
     console.log("Connection failled"+error);
